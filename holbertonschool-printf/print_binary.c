fb4d691ba3fbc5d6960dd105f79ce7ffe9a38070
JAuxance <auxance.jourdan@proton.me>
2025-12-02 17:02:53 +0100

    Add files via upload

---
+++ holbertonschool-printf/print_binary.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * print_binary - extracts an unsigned int from
+ * va_list and prints it in binary
+ * @n: the unsigned int to print
+ *
+ * Return: number of characters printed
+ */
+int print_binary(unsigned int n)
+{
+	int count = 0;
+	int i = 0;
+	int bits[32];
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	while (n > 0)
+	{
+		bits[i] = n % 2;
+		n = n / 2;
+		i++;
+	}
+
+	while (i--)
+	{
+		count += _putchar(bits[i] + '0');
+	}
+	return (count);
+}
+/**
+ * print_binary_path - path function to extract
+ * unsigned int from va_list
+ * @args: va_list containing the unsigned int
+ *
+ * Return: int
+ */
+int print_binary_path(va_list args)
+{
+	unsigned int n = va_arg(args, unsigned int);
+
+	return (print_binary(n));
+}
---
+++ holbertonschool-printf/print_percent.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * print_percent_path - prints a percent sign to standard output
+ *
+ * Return: int
+ */
+
+int print_percent_path(va_list args)
+{
+	(void)args;
+	_putchar('%');
+	return (1);
+}
